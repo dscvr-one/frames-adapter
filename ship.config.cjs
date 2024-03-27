e1a781e11891cd000aab232cb2caa9655b2c6d6a
Jimmy Madrigal Cruz <jimmymadrigal+github@gmail.com>
2024-03-26 20:07:22 -0600

    chore: release v2.0.2 (#6)
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>

--- ship.config.cjs
+++ ship.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   installCommand: () => 'npm ci --prefer-offline --no-audit',
   buildCommand: () => 'npm run build',
-  beforePublish: ({exec}) => exec('npm config set "//npm.pkg.github.com/:_authToken" "\${GITHUB_TOKEN}"'),
   publishCommand: ({ defaultCommand, tag }) =>
     `${defaultCommand} --access public`,
 };
